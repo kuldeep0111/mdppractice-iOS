08d347d7d9644f2e6dd603647530b663a158661b
Abhijeet <abhivjvm@lancersglobal.com>
2021-02-10 14:58:37 +0530

    add Holidays

--- mdpPractise/ViewControllers/Drawer/Holidays/AddHolidays.swift
+++ mdpPractise/ViewControllers/Drawer/Holidays/AddHolidays.swift
@@ -13,9 +13,26 @@ class AddHolidays: UIViewController {
     @IBOutlet weak var endDateTFD : MDPTextField!
     @IBOutlet weak var checkBtn : UIButton!
     @IBOutlet weak var saveBtn : UIButton!
+    
+    var startDatePicker = UIDatePicker()
+    var endDatePicker = UIDatePicker()
+    var pickerToolbar: UIToolbar?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         saveBtn.layer.cornerRadius = 25
+        createUIToolBar()
+        startDatePicker.datePickerMode = .date
+        startDatePicker.preferredDatePickerStyle = .wheels
+        startDateTFD?.inputAccessoryView = pickerToolbar
+        startDateTFD?.inputView = startDatePicker
+        //startDateTFD.delegate = self
+        
+        endDatePicker.datePickerMode = .date
+        endDatePicker.preferredDatePickerStyle = .wheels
+        endDateTFD?.inputAccessoryView = pickerToolbar
+        endDateTFD?.inputView = endDatePicker
+        //endDateTFD.delegate = self
     }
     
     @IBAction func didTapOnCheck(_ sender: UIButton){
@@ -30,3 +47,50 @@ class AddHolidays: UIViewController {
         
     }
 }
+
+
+//MARK: UIDatePicker
+extension AddHolidays {
+    func createUIToolBar() {
+            pickerToolbar = UIToolbar()
+            pickerToolbar?.autoresizingMask = .flexibleHeight
+
+            //customize the toolbar
+            pickerToolbar?.barStyle = .default
+            pickerToolbar?.barTintColor = UIColor.white
+            pickerToolbar?.backgroundColor = UIColor.white
+            pickerToolbar?.isTranslucent = false
+
+            //add buttons
+            let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action:
+                #selector(cancelBtnClicked(_:)))
+        cancelButton.tintColor = UIColor.black
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action:
+                #selector(doneBtnClicked(_:)))
+            doneButton.tintColor = UIColor.black
+
+            //add the items to the toolbar
+            pickerToolbar?.items = [cancelButton, flexSpace, doneButton]
+            
+        }
+        
+        @objc func cancelBtnClicked(_ button: UIBarButtonItem?) {
+            startDateTFD?.resignFirstResponder()
+            endDateTFD.resignFirstResponder()
+        }
+        
+        @objc func doneBtnClicked(_ button: UIBarButtonItem?) {
+            if(startDateTFD.isFirstResponder){
+                startDateTFD?.resignFirstResponder()
+                let formatter = DateFormatter()
+                formatter.dateStyle = .short
+                startDateTFD?.text = formatter.string(from: startDatePicker.date)
+            }else if(endDateTFD.isFirstResponder){
+                endDateTFD?.resignFirstResponder()
+                let formatter = DateFormatter()
+                formatter.dateStyle = .short
+                endDateTFD?.text = formatter.string(from: endDatePicker.date)
+            }
+        }
+    }
